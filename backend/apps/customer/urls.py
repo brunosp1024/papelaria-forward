f62c448251232efd5193693cc983f3c042ec1245
Bruno Salvador <bruno.pereira@visie.com.br>
2026-05-26 11:38:29 -0300

    feat(customer): add API routes

---
+++ backend/apps/customer/urls.py
@@ -0,0 +1,13 @@
+from rest_framework.routers import DefaultRouter
+from .views import CustomerViewSet
+
+
+class OptionalSlashRouter(DefaultRouter):
+    def __init__(self):
+        super().__init__()
+        self.trailing_slash = '/?'
+
+router = OptionalSlashRouter()
+router.register(r'customers', CustomerViewSet)
+
+urlpatterns = router.urls
