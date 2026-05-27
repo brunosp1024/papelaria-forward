e315d286f96c66b4e9ccc9f5150f4c351b11aa9c
Bruno Salvador <bruno.pereira@visie.com.br>
2026-05-27 01:57:58 -0300

    feat(sale): add SaleItem and CommissionConfig admin configurations

--- backend/apps/sale/admin.py
+++ backend/apps/sale/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models.sale import Sale
+from .models.sale_item import SaleItem
+from .models.commission_config import CommissionConfig
 
 
 @admin.register(Sale)
@@ -8,3 +10,15 @@ class SaleAdmin(admin.ModelAdmin):
     list_display = ('invoice_number', 'datetime', 'customer', 'seller')
     search_fields = ('invoice_number',)
     ordering = ('-datetime',)
+
+@admin.register(SaleItem)
+class SaleItemAdmin(admin.ModelAdmin):
+    list_display = ('sale', 'product', 'quantity')
+    search_fields = ('sale__invoice_number', 'product__name')
+    ordering = ('-sale__datetime',)
+
+@admin.register(CommissionConfig)
+class CommissionConfigAdmin(admin.ModelAdmin):
+    list_display = ('day_of_week', 'min_percentage', 'max_percentage')
+    search_fields = ('day_of_week',)
+    ordering = ('day_of_week',)
